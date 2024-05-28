bd74f61c4b0d5a9e58a8e99bebf9c831bf22d5eb
Ruoqi Huang <ruoqi.huang@myriad.com>
2024-05-27 21:45:22 -0700

    add chartjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { DisplayGraph } from './g';
+import { Timeline } from './b';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Timeline />
         <DisplayGraph />
       </header>
     </div>
---
+++ src/b.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  indexAxis: 'y',
+  borderSkipped: false,
+  scales: { x: {
+      min: 0,
+      max: 100,
+    }
+  },
+}
+
+const labels = [1,2,3,4,5];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+
+export const Timeline = () =>{
+  return (
+    <Bar
+      options={options}
+      data={data}
+    />
+  )
+}
